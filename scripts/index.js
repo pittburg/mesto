7fc352a0287c90cf85d6df9c897b120844f47c48
Andrey Aleksandrov <pittburg@yandex.ru>
2021-10-24 01:48:26 +0300

    refactor/ добавить комментарии к коду

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// находим значения в документе по классу и записываем в переменные
 const popup = document.querySelector('.popup');
 const popupEdit = document.querySelector('.popup_edit');
 const popupAdd = document.querySelector('.popup_add');
@@ -17,28 +18,27 @@ const popupModal = document.querySelector('.popup_modal');
 const popupModalCloseButton = document.querySelector('.popup__close_modal');
 
 
-
+// открывает попап добавляя класс 
 function openPopup(el) {
   el.classList.add('popup_opened');
 }
-
+// закрывает попап удаляя класс
 function closePopup(el) {
   el.classList.remove('popup_opened');
 }
-
-
+// открывает попап редактирования, подставляя значения профиля в поля инпутов
 function editPopup() {
-  openPopup(popupEdit);
-  fieldName.value = profileTitle.textContent;
-  fieldAbout.value = profileSubtitle.textContent;
+  openPopup(popupEdit); 
+  fieldName.value = profileTitle.textContent; 
+  fieldAbout.value = profileSubtitle.textContent; 
 }
-
+// открывает попап добавления карточек
 function addPopup() {
   openPopup(popupAdd);
   fieldPlace.value = '';
   fieldLink.value = '';
 }
-
+// отправляет форму редактирования, подставляя значения инпутов в поля профиля, закрывает попап
 function submitFormEdit(event) {
   event.preventDefault();
   profileTitle.textContent = fieldName.value;
@@ -46,7 +46,7 @@ function submitFormEdit(event) {
   closePopup(popupEdit);
  }
 
-
+// записывает массив в переменную
  const initialCards = [
   {
     name: 'Архыз',
@@ -74,9 +74,10 @@ function submitFormEdit(event) {
   }
 ];
 
-const gallery = document.querySelector('.cards-grid');
-const templateItem = document.querySelector('.template').content;
+const gallery = document.querySelector('.cards-grid'); // находит и записывает в переменную секцию для карточек
+const templateItem = document.querySelector('.template').content; // записывает в переменную содержимое тега template
 
+// перебор массива в обратном порядке и к каждому объекту применяется функция addCard
 initialCards.reverse().forEach(addCard);
 
 function likeToggle(event) {
@@ -87,21 +88,25 @@ function deleteCard(event) {
   event.target.closest('.card').remove();
 }; // удаление карточек
 
+// создание карточек - находит класс .card и клонирует со всем содержимым, подставляет значения заголовка и ссылку,
+// добавляет слушатели по клику на удаление карточки и переключение лайков
 function createCard(item){
   const card = templateItem.querySelector('.card').cloneNode(true);
   card.querySelector('.card__title').textContent = item.name;
   card.querySelector('.card__photo').src = item.link;
   card.querySelector('.card__trash').addEventListener('click', deleteCard);
   card.querySelector('.card__like').addEventListener('click', likeToggle);
-  
   return card;
 };
 
+// дабавляет карточки в начало галлереи
 function addCard(item){
   const card = createCard(item);
   gallery.prepend(card);
 };
 
+// отправляет форму добавления карточек, значения из инпутов записывает в переменные и передает как объект функции
+// добавления карточек, закрывает попап 
 function submitFormAdd(event) {
   event.preventDefault();
   const name = fieldPlace.value;
@@ -109,10 +114,11 @@ function submitFormAdd(event) {
   addCard({name, link});
   event.target.reset();
   closePopup(popupAdd);
- }
+ };
 
 const image = document.querySelector('.card__photo');
 
+// Модальное окно - по событию ищет значения для ссылки и заголовка, подставляет их в попап и открывает его
 function bigImage(event) {
   event.preventDefault();
   const caption = event.target.offsetParent.nextElementSibling.innerText;
@@ -120,15 +126,16 @@ function bigImage(event) {
   document.querySelector('.popup__photo').src = url;
   document.querySelector('.popup__caption').textContent = caption;
   openPopup(popupModal);
-}
+};
 
+// присваевает все элементы по тегу, перебирает их, слушает где был клик и запускает фунцию bigImage
 const images = document.getElementsByTagName('img');
   for (let i = 0; i < images.length; ++i) {
   let image = images[i];
   image.addEventListener('click', bigImage);
-}
+};
 
-
+// слушатели клика на открытие, закрытие попапов и отправки формы
 addButton.addEventListener('click', addPopup);
 editButton.addEventListener('click', editPopup);
 popupModalCloseButton.addEventListener('click', () => closePopup(popupModal));
@@ -136,3 +143,5 @@ popupEditCloseButton.addEventListener('click', () => closePopup(popupEdit));
 popupAddCloseButton.addEventListener('click', () => closePopup(popupAdd));
 formEdit.addEventListener('submit', submitFormEdit);
 formAdd.addEventListener('submit', submitFormAdd);
+
+// спасибо за код-ревью =)
