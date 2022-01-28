5349872ffbc6f6d8b307f38c29d0a898596aed77
Andrey Aleksandrov <pittburg@yandex.ru>
2022-01-28 15:09:40 +0300

    feature/ добавить класс Api, метод getUserInfo

---
+++ src/components/Api.js
@@ -0,0 +1,24 @@
+class Api {
+  constructor({baseUrl, headers}) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      headers: this._headers,
+    })
+    .then(this._checkResponse)
+  }
+
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`)
+  }
+
+}
+
+export default Api;
